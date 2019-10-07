ff41bb8c151ec7a458a128d4ba1112e5d206c800
Xxtractz <musambaloyi@gmail.com>
2019-10-07 17:19:12 +0200

    init day00

---
+++ ex00/ex00/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ex00
+//
+//  Created by Musa Martin BALOYI on 2019/10/07.
+//  Copyright © 2019 mbaloyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
