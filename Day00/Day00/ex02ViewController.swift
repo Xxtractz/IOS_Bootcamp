e8c8c72a338d1de7924221a84595302bb705fa97
xxtractz <musambaloyi@gmail.com>
2019-10-09 18:21:11 +0200

    Day00

--- Day00/Day00/ex02ViewController.swift
+++ Day00/Day00/ex02ViewController.swift
@@ -9,11 +9,97 @@
 import UIKit
 
 class ex02ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+        @IBAction func clear(_ sender: UIButton) {
+            print("Clear was Pressed")
+        }
+    
+        @IBAction func negative(_ sender: UIButton) {
+            print("negative/positive was Pressed")
+        }
+    
+        @IBAction func devide(_ sender: UIButton) {
+            print("devide was Pressed")
+        }
+    
+        @IBAction func multiply(_ sender: UIButton) {
+            print("multiply was Pressed")
+        }
+    
+        @IBAction func subtract(_ sender: UIButton) {
+            print("subtract was Pressed")
+        }
+    
+        @IBAction func add(_ sender: UIButton) {
+            print("add was Pressed")
+        }
+    
+        @IBAction func equal_to(_ sender: UIButton) {
+            print("equal_to was Pressed")
+        }
+    
+    
+        @IBAction func num9(_ sender: UIButton) {
+            print(9)
+            label.text = "9"
+        }
+    
+        @IBAction func num8(_ sender: UIButton) {
+            print(8)
+            label.text = "8"
+        }
+    
+        @IBAction func num7(_ sender: UIButton) {
+            print(7)
+            label.text = "7"
+        }
+    
+        @IBAction func num6(_ sender: UIButton) {
+            print(6)
+            label.text = "6"
+        }
+    
+        @IBAction func num5(_ sender: UIButton) {
+            print(5)
+            label.text = "5"
+        }
+    
+        @IBAction func num4(_ sender: UIButton) {
+            print(4)
+            label.text = "4"
+        }
+    
+        @IBAction func num3(_ sender: UIButton) {
+            print(3)
+            label.text = "3"
+        }
+    
+        @IBAction func num2(_ sender: UIButton) {
+            print(2)
+            label.text = "2"
+        }
+    
+        @IBAction func num1(_ sender: UIButton) {
+            print(1)
+            label.text = "1"
+        }
+    
+        @IBAction func num0(_ sender: UIButton) {
+            print(0)
+            label.text = "0"
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 
--- Day00/Day00/ex03ViewController.swift
+++ Day00/Day00/ex03ViewController.swift
@@ -9,11 +9,217 @@
 import UIKit
 
 class ex03ViewController: UIViewController {
+    
+    var Num_a = ""
+    var Num_b = ""
+    var firstNum = true
+    var previousOpperator = ""
+    var clear = false
+    let maxInt32 = Int32.max  // 2147483647
+    let minInt32 = Int32.min + 1  // -2147483648
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func clear(_ sender: UIButton) {
+        label.text = ""
+        firstNum = true
+    }
+
+    @IBAction func negative(_ sender: UIButton) {
+        if (label.text != "") {
+            if (label.text!.first == "-") {
+                label.text!.remove(at: label.text!.startIndex)
+            } else {
+                let newValue = "-" + label.text!
+                label.text = newValue
+            }
+        }
+    }
+
+    @IBAction func devide(_ sender: UIButton) {
+        if (previousOpperator == "") {
+            previousOpperator = "/"
+            Num_a = label.text!
+            clear = true
+            firstNum = true
+        } else {
+            previousOpperator = ""
+            clear = false
+        }
+    }
+
+    @IBAction func multiply(_ sender: UIButton) {
+        if (previousOpperator == "") {
+            previousOpperator = "x"
+            Num_a = label.text!
+            clear = true
+            firstNum = true
+        } else {
+            previousOpperator = ""
+            clear = false
+        }
+    }
+
+    @IBAction func subtract(_ sender: UIButton) {
+        if (previousOpperator == "") {
+            previousOpperator = "-"
+            Num_a = label.text!
+            clear = true
+            firstNum = true
+        } else {
+            previousOpperator = ""
+            clear = false
+        }
+    }
+
+    @IBAction func add(_ sender: UIButton) {
+        if (previousOpperator == "") {
+            previousOpperator = "+"
+            Num_a = label.text!
+            clear = true
+            firstNum = true
+        }
+        else {
+            previousOpperator = ""
+            clear = false
+        }
+    }
+
+    @IBAction func equal_to(_ sender: UIButton) {
+        if (label.text != "0") {
+            if (previousOpperator != "") {
+                let one = (Num_a as NSString).integerValue
+                let two = (label.text! as NSString).integerValue
+                if (one > maxInt32 || one < minInt32 || two > maxInt32 || two < minInt32) {
+                    label.text = "Error_Num"
+                } else {
+                    var result = 0
+                    if (previousOpperator == "+") {
+                        result = one + two
+                    } else if (previousOpperator == "-") {
+                        result = one - two
+                    } else if (previousOpperator == "/") {
+                        result = one / two
+                    } else if (previousOpperator == "x") {
+                        result = one * two
+                    }
+                    if (result > maxInt32 || result < minInt32) {
+                        label.text = "Error_Num"
+                    } else {
+                        label.text = String(result)
+                    }
+                }
+                previousOpperator = ""
+            }
+        } else {
+            previousOpperator = ""
+            label.text = "Error_Null"
+        }
+    }
+
+
+    @IBAction func num9(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "9"
+        firstNum = false
+    }
+
+    @IBAction func num8(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "8"
+        firstNum = false
+    }
+
+    @IBAction func num7(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "7"
+        firstNum = false
+    }
+
+    @IBAction func num6(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "6"
+        firstNum = false
+    }
+
+    @IBAction func num5(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "5"
+        firstNum = false
+    }
+
+    @IBAction func num4(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "4"
+        firstNum = false
+    }
+
+    @IBAction func num3(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "3"
+        firstNum = false
+    }
+
+    @IBAction func num2(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "2"
+        firstNum = false
+    }
+
+    @IBAction func num1(_ sender: UIButton) {
+        if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "1"
+        firstNum = false
+    }
+
+    @IBAction func num0(_ sender: UIButton) {
+       if (clear == true) {
+            label.text = ""
+            clear = false
+        }
+        label.text = label.text! + "0"
+        firstNum = false
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 
