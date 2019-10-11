38660c87b2cae19055bf95ecae1fe94487d29aac
Xxtractz <musambaloyi@gmail.com>
2019-10-11 12:02:46 +0200

    Day02

---
+++ Day02/Day02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day02
+//
+//  Created by Musa Martin BALOYI on 2019/10/11.
+//  Copyright © 2019 wethinkcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
